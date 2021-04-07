cb6bc9a1562873f6d285e4b780c82dbe039bd79d
Nelson Delgado <neslo890roj@gmail.com>
2021-04-07 18:54:02 -0500

    version 2

---
+++ contactos.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<link rel="icon" type="image/jpg" href="public/img/favicon.jpg">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Contactos Agenda</title>
+	<?php require_once "dependencias.php"; ?>
+</head>
+<body>
+	  <div class="container">
+	  	<?php require_once "menu.php"; ?>
+	  </div>
+
+
+	
+</body>
+</html>
