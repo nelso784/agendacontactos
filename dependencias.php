bde67b5aac4e4ba20f7e5708490cdb8e68d89a0a
Nelson Delgado <neslo890roj@gmail.com>
2021-04-07 17:57:41 -0500

    agregado de dependencias

---
+++ dependencias.php
@@ -0,0 +1,14 @@
+ 
+
+ <link rel="stylesheet" type="text/css" href="public/boostrap/bootstrap.min.css">
+
+ <link rel="stylesheet" type="text/css" href="public/fontawesome/css/all.css">
+  <link rel="stylesheet" type="text/css" href="public/datatable/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="public/datatable/dataTables.bootstrap4.min.css">
+
+ <script src="public/jquery-3.6.0.min.js"></script>
+ <script src="public/boostrap/popper.min.js"></script>
+ <script src="public/boostrap/bootstrap.min.js"></script>
+ <script src="public/datatable/jquery.dataTables.min.js"></script>
+ <script src="public/datatable/dataTables.bootstrap4.min.js"></script>
+ <script src="public/sweetalert.min.js"></script>
---
+++ menu.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<link rel="icon" type="image/jpg" href="public/img/favicon.jpg">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Agenda de Contactos web</title>
+	<?php require_once "dependencias.php"; ?>
+</head>
+<body>
+	
+</body>
+</html>
